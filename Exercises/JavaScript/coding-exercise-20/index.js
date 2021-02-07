6f4c79ae754b0d04ac9ee1f53d2feca04264b0e9
Kolbjorn Kelly <kolbjorn.kelly@gmail.com>
2021-02-07 12:42:56 +0100

    exercise 20 done

---
+++ Exercises/JavaScript/coding-exercise-20/index.js
@@ -0,0 +1,3 @@
+const die1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+const roll = `You rolled a ${die} and a ${die2}. They sum up to ${die1 + die2}`;
